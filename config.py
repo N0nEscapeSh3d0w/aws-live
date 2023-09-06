1a862e7037acdfdd959f267f1f8d62c667963450
zihui <101386735+N0nEscapeSh3d0w@users.noreply.github.com>
2023-09-07 00:16:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cjfu0ocm8ldv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "hingzihui-employee"
 customregion = "us-east-1"
 
